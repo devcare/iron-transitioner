bd00150461d9131324f1f716369cd14064b03056
Tom Coleman <tom@thesnail.org>
2014-03-20 23:01:45 +1100

    Transitioned Yield tests are working.

--- tests/transitioned_yield.js
+++ tests/transitioned_yield.js
@@ -32,41 +32,6 @@ var withTransitionedYieldInLayout = function(cb) {
   });
 }
 
-// *****************************************************
-
-// // XXX: could I just use a real layout?
-// var LayoutMock = UI.Component.extend({
-//   init: function() {
-//     this.dependency = new Deps.Dependency;
-//     this.template = null;
-//     this.data = null;
-//     
-//     this.regions = this._regions = new ReactiveDict;
-//   },
-//   
-//   setRegion: function(key, name) {
-//     
-//   }
-// });
-//   
-//   
-// PageManagerMock.prototype = {
-//   _renderTemplate: function() {
-//     this.dependency.depend();
-//     return Template[this.template](this.data);
-//   },
-//   
-//   setTemplate: function(template) {
-//     this.template = template;
-//     this.dependency.changed();
-//   },
-//   
-//   setData: function(data) {
-//     this.data = data;
-//     this.dependency.changed();
-//   }
-// }
-
 Tinytest.add('TransitionedYield - standard Layout - basic rendering', function (test) {
   withTransitionedYieldInLayout(function(screen, layout) {
     layout.setRegion('one');
@@ -99,91 +64,73 @@ Tinytest.add('TransitionedYield - standard Layout - basic transitioning', functi
   });
 });
 
-// 
-// Tinytest.add('TransitionedYield - reactivity', function (test) {
-//   var pageManager = new PageManagerMock()
-//   var transitionedYield = new TransitionedYield(pageManager, '__main__');
-//   
-//   var frag = Spark.render(function() {
-//     return transitionedYield.render();
-//   });
-//   var div = new OnscreenDiv(frag);
-//   
-//   pageManager.setTemplate('oneData');
-//   pageManager.setData({property: 'first'});
-//   Deps.flush();
-//   test.equal(div.text().trim(), '1-first', 'one not rendered with data');
-//   
-//   transitionedYield.transition('twoData', 'normal');
-//   pageManager.setTemplate('twoData');
-//   pageManager.setData({property: 'second'});
-//   Deps.flush();
-//   test.matches(div.text().trim(), /1-first\s+2-second/, 'two not rendered alongside');
-//   
-//   transitionedYield.stopTransition();
-//   pageManager.setTemplate('twoData');
-//   Deps.flush();
-//   test.equal(div.text().trim(), '2-second', 'two not rendered');
-// });
-// 
-// Tinytest.add('TransitionedYield - cleanup', function (test) {
-//   var pageManager = new PageManagerMock()
-//   var transitionedYield = new TransitionedYield(pageManager, '__main__');
-//   
-//   var frag = Spark.render(function() {
-//     return transitionedYield.render();
-//   });
-//   var div = new OnscreenDiv(frag);
-//   
-//   var calls = 0;
-//   Template.oneData.rendered = function() { calls += 1 }
-//   
-//   pageManager.setTemplate('oneData');
-//   pageManager.setData({property: 'first'});
-//   Deps.flush();
-//   test.equal(div.text().trim(), '1-first', 'one not rendered with data');
-//   var made_calls = calls;
-//   
-//   transitionedYield.transition('twoData', 'normal');
-//   pageManager.setTemplate('twoData');
-//   pageManager.setData({property: 'second'});
-//   Deps.flush();
-//   test.matches(div.text().trim(), /1-first\s+2-second/, 'two not rendered alongside');
-//   test.equal(made_calls, calls);
-//   
-//   pageManager.setData({property: 'third'});
-//   Deps.flush();
-//   test.equal(made_calls, calls);
-//   
-//   transitionedYield.stopTransition();
-//   Deps.flush();
-//   test.equal(made_calls, calls);
-// });
-// 
-// Tinytest.add('TransitionedYield - classes set', function (test) {
-//   var pageManager = new PageManagerMock()
-//   var transitionedYield = new TransitionedYield(pageManager, '__main__');
-//   
-//   var frag = Spark.render(function() {
-//     return transitionedYield.render();
-//   });
-//   var div = new OnscreenDiv(frag);
-//   
-//   pageManager.setTemplate('one');
-//   Deps.flush();
-//   
-//   transitionedYield.transition('normal');
-//   pageManager.setTemplate('two');
-//   Deps.flush();
-//   
-//   var classes = div.div.children[0].className;
-//   test.matches(classes, /normal/, 'No class set on div');
-//   test.matches(classes, /transitioning/, 'No transitioning set on div');
-//   
-//   transitionedYield.stopTransition();
-//   var classes = div.div.children[0].className;
-//   Deps.flush();
-//   
-//   var classes = div.div.children[0].className;
-//   test.equal(classes, 'transitioner-panes', 'Classes not cleared');
-// });
+
+Tinytest.add('TransitionedYield - standard Layout - reactivity', function (test) {
+  withTransitionedYieldInLayout(function(screen, layout, transitionedYield) {
+    layout.setRegion('oneData');
+    layout.setData({property: 'first'})
+    Deps.flush();
+    test.equal(screen.text().trim(), '1-first', 'one not rendered with data');
+  
+    transitionedYield.transition('twoData', 'normal');
+    layout.setRegion('twoData');
+    layout.setData({property: 'second'});
+    Deps.flush();
+    test.matches(screen.text().trim(), /1-first\s+2-second/, 'two not rendered alongside');
+  
+    transitionedYield.stopTransition();
+    layout.setRegion('twoData');
+    Deps.flush();
+    test.equal(screen.text().trim(), '2-second', 'two not rendered');
+  });
+});
+
+Tinytest.add('TransitionedYield - standard Layout - cleanup', function (test) {
+  withTransitionedYieldInLayout(function(screen, layout, transitionedYield) {
+    var calls = 0;
+    Template.oneData.rendered = function() { calls += 1 }
+  
+    layout.setRegion('oneData');
+    layout.setData({property: 'first'});
+    Deps.flush();
+    test.equal(screen.text().trim(), '1-first', 'one not rendered with data');
+    var made_calls = calls;
+  
+    transitionedYield.transition('twoData', 'normal');
+    layout.setRegion('twoData');
+    layout.setData({property: 'second'});
+    Deps.flush();
+    test.matches(screen.text().trim(), /1-first\s+2-second/, 'two not rendered alongside');
+    test.equal(made_calls, calls);
+  
+    layout.setData({property: 'third'});
+    Deps.flush();
+    test.equal(made_calls, calls);
+  
+    transitionedYield.stopTransition();
+    Deps.flush();
+    test.equal(made_calls, calls);
+  });
+});
+
+Tinytest.add('TransitionedYield - standard Layout - classes set', function (test) {
+  withTransitionedYieldInLayout(function(screen, layout, transitionedYield) {
+    layout.setRegion('one');
+    Deps.flush();
+  
+    transitionedYield.transition('normal');
+    layout.setRegion('two');
+    Deps.flush();
+  
+    var classes = screen.div.children[0].className;
+    test.matches(classes, /normal/, 'No class set on div');
+    test.matches(classes, /transitioning/, 'No transitioning set on div');
+  
+    transitionedYield.stopTransition();
+    var classes = screen.div.children[0].className;
+    Deps.flush();
+  
+    var classes = screen.div.children[0].className;
+    test.equal(classes, 'transitioner-panes', 'Classes not cleared');
+  });
+});
