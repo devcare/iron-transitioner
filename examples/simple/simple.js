0e910fd45c3a1cb2d6a85356ff7bfd1c73bc3180
Tom Coleman <tom@thesnail.org>
2014-03-31 15:46:52 +1100

    Alright, pretty much seems to be working with blaze.
    Woo.

--- examples/simple/simple.js
+++ examples/simple/simple.js
@@ -1,4 +1,9 @@
+if (Meteor.isClient) {
+  Session.setDefault('one', 'one');
+  Session.setDefault('two', 'two');
+}
+
 Router.map(function() {
-  this.route('one', {path: '/'});
-  this.route('two');
+  this.route('one', {path: '/', data: function() { return Session.get('one'); }});
+  this.route('two', {data: function() { return Session.get('two'); }});
 });
